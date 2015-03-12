deafcb66973e8dbff1ea0586de26b345cafb51b1
Simon Pay <simonpay@yahoo.co.uk>
2015-03-12 11:25:13 +0000

    Wrote content for file explorer single and split views. Refactored SCSS structure in app.scss. Added shame.scss

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,10 @@
 
 	var $drawerWrap = $( ".drawer-wrap" ),
 		$actionsWrap = $( ".actions-wrap" );
-
-	// $( ".tmp-buttons--drawer .toggle" ).click(function() {
+	// $( ".tmp-buttons--drawer .js-toggle" ).click(function() {
 	// 	$drawerWrap.toggleClass( "is-hidden" );
 	// });
-	// $( ".tmp-buttons--actions .toggle" ).click(function() {
+	// $( ".tmp-buttons--actions .js-toggle" ).click(function() {
 	// 	$actionsWrap.toggleClass( "is-hidden" );
 	// });
 	$( ".main-nav__link.-files" ).click(function() {
@@ -19,11 +18,23 @@
 	$( ".table__row:not(.-header)" ).on( "click", function(){
 		$(this).toggleClass( "is-selected" );
 		$actionsWrap.toggleClass( "is-hidden" );
+		$( ".main-container" ).toggleClass( "actions-is-open" );
 	});
 
 	$( ".drawer__link" ).on( "click", function(){
 		$(this).toggleClass( "is-selected" );
 	});
+
+
+	var setFileExplorerView = function (obj) {
+		$( ".table-wrap-outer" ).hide();
+		obj.show();
+	};
+	
+	$( ".js-buttons-files .button" ).on( "click", function(){
+		setFileExplorerView($( $(this).data("class") ));
+	});
+	setFileExplorerView($( ".-cols-1" ));
 
 
 })();
